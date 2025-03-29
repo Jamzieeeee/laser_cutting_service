34f3a015be17ca7daf7488d33ae263060b792a08
Jamzieeeee <james@altrion.org>
2025-03-29 20:12:57 +0000

    Add bases view and html template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Base
 
 # Create your views here.
+def bases(request):
+    products = Base.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/bases.html', context)
