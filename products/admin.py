1c2d5070814a975438789fd3dd2973a8a614bacb
Jamzieeeee <james@altrion.org>
2025-03-29 14:29:10 +0000

    Customize product's admin view

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Base, Material
 
 # Register your models here.
-admin.site.register(Base)
-admin.site.register(Material)
+
+
+class BaseAdmin(admin.ModelAdmin):
+    list_display = (
+        'size',
+        'number_per_sheet',
+    )
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'cost_per_sheet',
+    )
+
+
+admin.site.register(Base, BaseAdmin)
+admin.site.register(Material, MaterialAdmin)
