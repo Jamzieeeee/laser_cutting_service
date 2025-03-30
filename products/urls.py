b16cf5ffb61dbffaabc7f62e5e0e8e39cc7c9b4a
Jamzieeeee <james@altrion.org>
2025-03-30 17:10:53 +0100

    Add materials page and navigation

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('bases/', views.bases, name='bases'),
     path('bases/<base_id>', views.base_detail, name='base_detail'),
+    path('materials/', views.materials, name='materials'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Base
+from .models import Base, Material
 
 # Create your views here.
 def bases(request):
@@ -20,3 +20,13 @@ def base_detail(request, base_id):
     }
 
     return render(request, 'products/base_detail.html', context)
+
+
+def materials(request):
+    materials = Material.objects.all()
+
+    context = {
+        'products': materials,
+    }
+
+    return render(request, 'products/materials.html', context)
