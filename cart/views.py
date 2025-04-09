a6be1d604a6caad86630c536c037ee30e1574378
Jamzieeeee <james@altrion.org>
2025-04-09 23:05:57 +0100

    Add update and delete functionality to cart page

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def cart(request):
+    if request.method=="POST":
+        params = request.POST
+        cart = request.session.get('cart', {})
+
+
+        for field in params:
+            field_parts = field.split("-")
+            print(field)
+
+            if field_parts[0]=="quantity":
+                base_id = field_parts[1]
+                material_id = field_parts[2]
+                quantity = int(params[field])
+                if quantity == 0:
+                    del cart[base_id][material_id]
+                else:
+                    cart[base_id][material_id] = quantity
+                
+                request.session['cart'] = cart
+
+
     return render(request, 'cart/cart.html')
 
 def add_to_cart(request, item_id, material_id):
