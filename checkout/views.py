bac3b90829df6999110ea2bc681d18d8c2b03c51
Jamzieeeee <james@altrion.org>
2025-04-20 20:30:10 +0100

    Add card input

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, reverse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.contrib import messages
 from .forms import OrderForm
 
 # Create your views here.
+@csrf_exempt
+
 def checkout(request):
     cart = request.session.get('cart', {})
     if not cart:
@@ -13,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RCSZkBCvEtEjVCmxVBHXC2TrfQhqhAefC1jNYwHRbYe5YgbuRHVdgzUgv559ghcdQEgXheRW4fYgizx95WgI5EI00vyiXph76',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
