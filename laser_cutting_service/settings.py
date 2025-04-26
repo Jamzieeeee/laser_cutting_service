b9221883bd0a50a66895e6f9a051ecf2ec57b754
Jamzieeeee <james@altrion.org>
2025-04-26 18:06:57 +0100

    Add checkout form data caching

--- laser_cutting_service/settings.py
+++ laser_cutting_service/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-4)bexlp#p+cbf+v90fr&%0v^=c&m$kft!5(f29g@d-u@u5ic#0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', # vs code preview
+    'localhost', # listen for stripe webhooks
+]
 
 
 # Application definition
@@ -170,3 +173,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
